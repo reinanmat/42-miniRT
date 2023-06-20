07430d10e2cd0a4d2aa76407a9c57c41c0dcad4f
reinanmat <reinan266@gmail.com>
2023-06-20 18:14:54 -0300

    feat: init camera struct

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,39 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 11:11:03 by revieira          #+#    #+#             */
-/*   Updated: 2023/06/20 12:26:18 by revieira         ###   ########.fr       */
+/*   Updated: 2023/06/20 18:07:11 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
+
+t_point	get_lower_left_corner(t_cam cam)
+{
+	t_point	lower_left_corner;
+	t_point	half_vertical;
+	t_point	half_horizontal;
+	t_point	aux;
+
+	half_vertical = division_n(cam.vertical, 2);
+	half_horizontal = division_n(cam.horizontal, 2);
+	aux = minus(minus(cam.origin, half_horizontal), half_vertical);
+	lower_left_corner = minus(aux, (t_point){0, 0, cam.focal_length});
+	return (lower_left_corner);
+}
+
+t_cam	init_cam(void)
+{
+	t_cam	cam;
+
+	cam.viewport_height = 4.0;
+	cam.viemport_width = cam.viewport_height * ASPECT_RADIO;
+	cam.focal_length = 1.0;
+	cam.origin = (t_point){0, 0, 0};
+	cam.horizontal = (t_point){cam.viemport_width, 0, 0};
+	cam.vertical = (t_point){0, cam.viewport_height, 0};
+	cam.lower_left_corner = get_lower_left_corner(cam);
+	return (cam);
+}
 
 t_mlx	init_mlx(void)
 {
@@ -24,9 +52,10 @@ t_mlx	init_mlx(void)
 
 int	main(void)
 {
-	t_mlx	mlx;
+	t_data	data;
 
-	mlx = init_mlx();
-	render(mlx);
-	mlx_loop(mlx.mlx_ptr);
+	data.mlx = init_mlx();
+	data.cam = init_cam();
+	render(data);
+	mlx_loop(data.mlx.mlx_ptr);
 }
