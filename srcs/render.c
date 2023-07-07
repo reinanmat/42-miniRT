d131a4efb00451b74487f626128e8f5be62d27d2
reinanmat <reinan266@gmail.com>
2023-07-07 14:13:05 -0300

    fix: types of functions and variables

--- srcs/render.c
+++ srcs/render.c
@@ -6,16 +6,16 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:25:59 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/21 12:58:53 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/21 13:01:07 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
-t_point	ray_color(t_ray ray)
+t_color	ray_color(t_ray ray)
 {
-	t_point	direction;
-	t_point	color;
+	t_vec3	direction;
+	t_color	color;
 	t_point	aux1;
 	t_point	aux2;
 	double	t;
@@ -48,18 +48,18 @@ t_ray	get_ray(double u, double v, t_cam cam)
 	return (ray);
 }
 
-t_point	calculate_point_color(int x, int y, t_cam cam, int height)
+t_color	calculate_ray_color(int x, int y, t_cam cam)
 {
 	double	u;
 	double	v;
 	t_ray	ray;
-	t_point	point_color;
+	t_color	color;
 
 	u = (double)x / (WIDTH - 1);
 	v = (double)y / (height - 1);
 	ray = get_ray(u, v, cam);
-	point_color = ray_color(ray);
-	return (point_color);
+	color = ray_color(ray);
+	return (color);
 }
 
 void	render_background(t_cam cam, t_mlx mlx, int height)
@@ -68,7 +68,7 @@ void	render_background(t_cam cam, t_mlx mlx, int height)
 	int		y;
 	int		i;
 	int		j;
-	t_point	point_color;
+	t_color	ray_color;
 
 	j = height - 1;
 	y = 0;
@@ -78,8 +78,8 @@ void	render_background(t_cam cam, t_mlx mlx, int height)
 		x = 0;
 		while (i < WIDTH - 1)
 		{
-			point_color = calculate_point_color(i, j, cam, height);
-			mlx_img_pix_put(&mlx.img, x, y, get_color(point_color));
+			ray_color = calculate_ray_color(i, j, cam);
+			mlx_img_pix_put(&mlx.img, x, y, get_color(ray_color));
 			x++;
 			i++;
 		}
