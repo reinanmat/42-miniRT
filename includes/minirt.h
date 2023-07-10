05962ddd4a03be694ada78bfb378186130650657
reinanmat <reinan266@gmail.com>
2023-07-10 11:42:14 -0300

    feat: added world structure
    this struct holds camera, lights and objects

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:32:12 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/07 14:16:41 by revieira         ###   ########.fr       */
+/*   Updated: 2023/07/10 13:26:41 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,7 @@ typedef struct s_mlx
 
 typedef struct s_cam
 {
+	double	fov;
 	double	viewport_height;
 	double	viewport_width;
 	double	focal_length;
@@ -72,10 +73,50 @@ typedef struct s_ray
 	t_vec3	direction;
 }	t_ray;
 
+typedef struct s_light
+{
+	t_color	color;
+	t_point	position;
+	double	brightness;
+}	t_light;
+
+typedef struct s_sphere
+{
+	t_point center;
+	double	radius;
+	t_color	color;
+}	t_sphere;
+
+typedef struct s_cylinder
+{
+	t_point center;
+	t_vec3	vector;
+	double	radius;
+	double	height;
+	t_color	color;
+}	t_cylinder;
+
+typedef struct s_plane
+{
+	t_point	center;
+	t_vec3	vector;
+	t_color	color;
+}	t_plane;
+
+typedef struct s_world
+{
+	t_cam		cam;
+	t_light		light;
+	t_light		ambient_light;
+	t_sphere	*sp;
+	t_cylinder	*cy;
+	t_plane		*pl;
+}	t_world;
+
 typedef struct s_data
 {
 	t_mlx	mlx;
-	t_cam	cam;
+	t_world	world;
 }	t_data;
 
 //color
