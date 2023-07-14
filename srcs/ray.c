3fca12a4fe56619b15a391daf58ff4307909240f
reinanmat <reinan266@gmail.com>
2023-07-14 18:57:02 -0300

    feat: added other versions of ray_color function with object reflection

--- srcs/ray.c
+++ srcs/ray.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 13:28:55 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/14 18:49:16 by revieira         ###   ########.fr       */
+/*   Updated: 2023/07/14 18:55:41 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,52 @@ t_color	ray_color(t_ray ray, t_world world)
 			(t_color){0.5, 0.7, 1.0});
 	return (color);
 }
+
+/* t_vec3	reflection(t_vec3 direction, t_vec3 normal) */
+/* { */
+/* 	double	aux; */
+/* 	t_vec3	reflect; */
+
+/* 	aux = -2 * dot(direction, normal); */
+/* 	reflect = s_multiply(normal, aux); */
+/* 	return (reflect); */
+/* } */
+
+/* t_color	ray_color(t_ray ray, t_world world, int depth) */
+/* { */
+/* 	t_ray	new_ray; */
+/* 	t_color	color; */
+/* 	t_point	target; */
+/* 	t_hit	hit; */
+
+/* 	if (depth == 0) */
+/* 		return ((t_color){0, 0, 0}); */
+/* 	if (hit_anything(world.objects, ray, 0.01, INFINITY, &hit)) */
+/* 	{ */
+/* 		target = add(add(hit.point, hit.normal), vec_rand()); */
+/* 		new_ray.origin = hit.point; */
+/* 		new_ray.direction = normalize(minus(target, hit.point)); */
+/* 		return (s_multiply(ray_color(new_ray, world, depth - 1), 0.5)); */
+/* 	} */
+/* 	color = gradient_color(ray.direction.y, (t_color){1.0, 1.0, 1.0}, */
+/* 			(t_color){0.5, 0.7, 1.0}); */
+/* 	return (color); */
+/* } */
+
+/* t_color	ray_color(t_ray ray, t_world world) */
+/* { */
+/* 	t_ray	new_ray; */
+/* 	t_color	color; */
+/* 	t_hit	hit; */
+
+/* 	new_ray.origin = ray.origin; */
+/* 	new_ray.direction = ray.direction; */
+/* 	while (hit_anything(world.objects, new_ray, 0.01, INFINITY, &hit)) */
+/* 	{ */
+/* 		new_ray.origin = hit.point; */
+/* 		new_ray.direction = normalize(reflection(ray.direction, hit.normal)); */
+/* 	} */
+/* 	color = gradient_color(new_ray.direction.y, (t_color){1.0, 1.0, 1.0}, */
+/* 			(t_color){0.5, 0.7, 1.0}); */
+/* 	return (color); */
+/* } */
