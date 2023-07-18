109476ab29b1017920c018a3ed68fac622a15ea9
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-18 19:20:50 -0300

    feat: get_file_content

--- srcs/file_parsing/parse.c
+++ srcs/file_parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 21:04:20 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/07/07 16:58:23 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/18 19:14:31 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
@@ -33,6 +33,26 @@ int	file_ends_with_rt(char *filename)
 	else if (ft_strcmp(file_extension, ".rt") != 0)
 		return (0);
 	return (1);
+}
+
+char	*get_file_content(int fd, char *filename)
+{
+	int		i;
+	int		new_fd;
+	char	buf[1];
+	char	*file_content;
+
+	i = 0;
+	while (read(fd, buf, 1))
+		i++;
+	if (i == 0)
+		return (NULL);
+	file_content = malloc(sizeof(char) * (i + 1));
+	new_fd = reset_fd(fd, filename);
+	read(new_fd, file_content, i);
+	file_content[i] = '\0';
+	reset_fd(fd, filename);
+	return (file_content);
 }
 
 int	received_invalid_param(char *filename)
