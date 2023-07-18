c673533982dff66336f2779548788ef77369e967
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-18 19:46:35 -0300

    feat: ft_putstr returns an exit code now

--- libft/ft_putstr.c
+++ libft/ft_putstr.c
@@ -6,16 +6,17 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:16:30 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/28 16:17:26 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/18 18:16:19 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	ft_putstr(char *str)
+int	ft_putstr(char *str, int exit_code)
 {
 	while (*str)
 	{
 		write(1, str, 1);
 		str++;
 	}
+	return (exit_code);
 }
