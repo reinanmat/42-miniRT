0c42180ca7adc411849bfbf713c9e3ff592d514a
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-20 16:58:24 -0300

    refactor: is_number changed to test float numbers too

--- srcs/file_parsing/parse_aux_functions/is_number.c
+++ srcs/file_parsing/parse_aux_functions/is_number.c
@@ -6,21 +6,35 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 17:31:28 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/07/18 17:34:01 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/20 16:47:55 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
 
-int	is_number(const char *str)
+int is_number(const char *str)
 {
-	int	i;
+	int digit_count;
+	int	post_dot_count;
 
-	i = 0;
-	while (str[i])
+	digit_count = 0;
+	post_dot_count = 0;
+	if (count_dots(str) > 1)
+		return (0);
+	if (*str == '+' || *str == '-')
+		str++;
+	while (ft_isdigit(*str))
 	{
-		if (!ft_isdigit(str[i]) && (str[i] != '+' && str[i] != '-'))
-			return (0);
-		i++;
+		digit_count++;
+		str++;
 	}
+	if (*str == '.')
+		str++;
+	while (ft_isdigit(*str))
+	{
+		post_dot_count++;
+		str++;
+	}
+	if (post_dot_count == 0 || digit_count == 0 || *str != '\0')
+		return (0);
 	return (1);
 }
