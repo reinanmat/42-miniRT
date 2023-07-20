eb3ebfb915fdf6938d684898a276395d53922e3d
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-20 17:08:27 -0300

    fix: if replaced for else-if

--- srcs/file_parsing/parse_aux_functions/is_valid_ambient_lighting.c
+++ srcs/file_parsing/parse_aux_functions/is_valid_ambient_lighting.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:51:36 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/07/20 17:07:18 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/20 17:08:15 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
@@ -17,7 +17,7 @@ int	is_valid_ambient_lighting(char **params, int number_of_params)
 		return (ft_putstr("Error\n", 0));
 	else if (!is_valid_lighting_ratio(params[1]))
 		return (0);
-	if (!is_valid_rgb_set(params[2]))
+	else if (!is_valid_rgb_set(params[2]))
 		return (0);
 	return (1);
 }
