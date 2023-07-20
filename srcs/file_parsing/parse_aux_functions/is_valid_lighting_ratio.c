28f4c1c69d2e9507562215d946b44934c8934fab
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-20 17:04:35 -0300

    feat: better error messages lighting ratio

--- srcs/file_parsing/parse_aux_functions/is_valid_lighting_ratio.c
+++ srcs/file_parsing/parse_aux_functions/is_valid_lighting_ratio.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 18:22:05 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/07/20 15:01:14 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/20 17:03:43 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
@@ -16,9 +16,9 @@ int	is_valid_lighting_ratio(const char *str)
 	double	ratio;
 
 	if (!is_number(str))
-		return (0);
+		return (ft_putstr("[LIGHT RATIO] Not a number\n", 0));
 	ratio = ft_atof(str);
 	if (ratio < 0.0 || ratio > 1.0)
-		return (ft_putstr("[LIGHT RATIO] Set not between expected range\n", 0));
+		return (ft_putstr("[LIGHT RATIO] Not between expected range\n", 0));
 	return (1);
 }
