883473acccf0fc73489b8575ccfe181488c0a7fe
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-20 19:05:07 -0300

    feat: is_valid_light

--- srcs/file_parsing/parse_aux_functions/is_valid_light.c
+++ srcs/file_parsing/parse_aux_functions/is_valid_light.c
@@ -6,14 +6,18 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:56:19 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/07/18 18:59:36 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/20 19:03:41 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
 
 int	is_valid_light(char **params, int number_of_params)
 {
-	if (params && number_of_params)
-	{}
+	if (number_of_params != 2)
+		return (ft_putstr("Error\n", 0));
+	else if (!is_valid_coordinates(params[1]))
+		return (0);
+	else if (!is_valid_lighting_ratio(params[2]))
+		return (0);
 	return (1);
 }
