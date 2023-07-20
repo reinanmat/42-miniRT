4a1eb7b552364474f2fdbe2a6301d79263ae206e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-20 15:06:36 -0300

    feat: using is_valid_lighting_ratio

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:32:12 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/18 19:58:30 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/20 14:59:10 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,5 +125,6 @@ int		is_valid_cylinder(char **params, int number_of_params);
 int		is_number(const char *str);
 int		set_contain_only_numbers(const char *str);
 int		is_valid_rgb_set(const char *str);
+int		is_valid_lighting_ratio(const char *str);
 
 #endif
