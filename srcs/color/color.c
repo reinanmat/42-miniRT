cc4bca93cdc6710e8e570ffb1fb28f59aa939922
reinanmat <reinan266@gmail.com>
2023-07-20 14:39:17 -0300

    refactor: creating a directory to hold functions related to color

--- srcs/color/color.c
+++ srcs/color/color.c
@@ -6,11 +6,11 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:25:05 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/14 18:27:05 by revieira         ###   ########.fr       */
+/*   Updated: 2023/07/20 13:25:21 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minirt.h"
+#include "../../includes/minirt.h"
 
 int	get_color(t_color color)
 {
@@ -22,18 +22,4 @@ int	get_color(t_color color)
 	g = color.y * 255.999;
 	b = color.z * 255.999;
 	return (r << 16 | g << 8 | b);
-}
-
-t_color	gradient_color(double axis, t_color a, t_color b)
-{
-	double	t;
-	t_color	aux1;
-	t_color	aux2;
-	t_color	color;
-
-	t = 0.5 * (axis + 1.0);
-	aux1 = s_multiply(a, (1.0 - t));
-	aux2 = s_multiply(b, t);
-	color = add(aux1, aux2);
-	return (color);
 }
