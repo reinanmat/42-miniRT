b33cf74fa698aaa89ac304d7ca6a16af7b723f2e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-20 14:56:54 -0300

    fix: putstr was not using the correct exit_code

--- srcs/file_parsing/parse_aux_functions/is_valid_rgb_set.c
+++ srcs/file_parsing/parse_aux_functions/is_valid_rgb_set.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 18:25:07 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/07/18 19:01:14 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/20 14:43:19 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
@@ -49,11 +49,11 @@ static int	between_expected_range(const char *str)
 int	is_valid_rgb_set(const char *str)
 {
 	if (!expected_number_of_params(str))
-		return (ft_putstr("[RGB] Unexpected number of params\n", 1));
+		return (ft_putstr("[RGB] Unexpected number of params\n", 0));
 	else if (!set_contain_only_numbers(str))
-		return (ft_putstr("[RGB] Set does not contain only numbers\n", 1));
+		return (ft_putstr("[RGB] Set does not contain only numbers\n", 0));
 	else if (!between_expected_range(str))
-		return (ft_putstr("[RGB] Set not between expected range\n", 1));
+		return (ft_putstr("[RGB] Set not between expected range\n", 0));
 	else
 		return (1);
 }
