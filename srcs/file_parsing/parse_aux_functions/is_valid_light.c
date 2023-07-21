59af32758377838a306a44cc9209b575344498e6
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-21 12:33:42 -0300

    fix: minor fixes on each type parsed

--- srcs/file_parsing/parse_aux_functions/is_valid_light.c
+++ srcs/file_parsing/parse_aux_functions/is_valid_light.c
@@ -6,14 +6,14 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:56:19 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/07/20 19:03:41 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/21 12:23:57 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
 
 int	is_valid_light(char **params, int number_of_params)
 {
-	if (number_of_params != 2)
+	if (number_of_params != 3)
 		return (ft_putstr("Error\n", 0));
 	else if (!is_valid_coordinates(params[1]))
 		return (0);
