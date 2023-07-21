080c15699f94e951501e665696f37edca2d59f0b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-21 12:14:55 -0300

    feat: is_valid_plane

--- srcs/file_parsing/parse_aux_functions/is_valid_plane.c
+++ srcs/file_parsing/parse_aux_functions/is_valid_plane.c
@@ -6,14 +6,20 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:58:35 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/07/18 18:59:42 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/21 12:14:44 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
 
 int	is_valid_plane(char **params, int number_of_params)
 {
-	if (params && number_of_params)
-	{}
+	if (number_of_params != 3)
+		return (ft_putstr("Error\n", 0));
+	else if (!is_valid_coordinates(params[1]))
+		return (0);
+	else if (!is_valid_3d_normalized_vec(params[2]))
+		return (0);
+	else if (!is_valid_rgb_set(params[3]))
+		return (0);
 	return (1);
 }
