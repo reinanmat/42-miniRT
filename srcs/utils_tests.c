bda459238014c2b2c96e0eb5fe473e2a55180974
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-25 19:03:55 -0300

    refactor: removing unused function

--- srcs/utils_tests.c
+++ srcs/utils_tests.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 12:22:10 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/11 16:26:47 by revieira         ###   ########.fr       */
+/*   Updated: 2023/07/25 18:43:54 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,4 @@ void	print_hittable(t_hittable **hittable)
 void	print_point(t_point point)
 {
 	printf("%f %f %f\n", point.x, point.y, point.z);
-}
-
-void	print_cam(t_cam cam)
-{
-	printf("\n");
-	printf("origin: ");
-	print_point(cam.origin);
-	printf("horizontal: ");
-	print_point(cam.horizontal);
-	printf("vertical: ");
-	print_point(cam.vertical);
-	printf("lower_left_corner: ");
-	print_point(cam.lower_left_corner);
 }
