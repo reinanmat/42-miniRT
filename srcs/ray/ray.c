21124c1509794d02c4bfa0ee0b04b7d2475ad29a
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-25 19:03:33 -0300

    refactor: variables on the struct were renamed

--- srcs/ray/ray.c
+++ srcs/ray/ray.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 13:08:11 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/20 13:25:46 by revieira         ###   ########.fr       */
+/*   Updated: 2023/07/25 18:43:41 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_ray	get_ray(double u, double v, t_cam cam)
 	t_vec3	direction;
 
 	aux1 = add(cam.lower_left_corner, s_multiply(cam.horizontal, u));
-	aux2 = minus(s_multiply(cam.vertical, v), cam.origin);
+	aux2 = minus(s_multiply(cam.vertical, v), cam.coordinate);
 	direction = normalize(add(aux1, aux2));
-	return (ray(cam.origin, direction));
+	return (ray(cam.coordinate, direction));
 }
