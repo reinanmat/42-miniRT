f32e8575c2d655e58ddb1e2d6947d7e939b3c255
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-25 19:51:51 -0300

    feat: enhancing test that prints linked list

--- srcs/utils_tests.c
+++ srcs/utils_tests.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 12:22:10 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/25 18:43:54 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/25 19:46:34 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,24 @@ void	print_hittable(t_hittable **hittable)
 		if (aux->type == 1)
 		{
 			print_point(aux->sp->center);
+			printf("%f\n", aux->sp->radius * 2);
 			print_point(aux->sp->color);
-			printf("%f\n", aux->sp->radius);
 		}
+		else if (aux->type == 2)
+		{
+			print_point(aux->cy->center);
+			print_point(aux->cy->vector);
+			printf("%f\n", aux->cy->diameter);
+			printf("%f\n", aux->cy->height);
+			print_point(aux->cy->color);
+		}
+		else if (aux->type == 3)
+		{
+			print_point(aux->pl->center);
+			print_point(aux->pl->vector);
+			print_point(aux->pl->color);
+		}
+		printf("\n\n");
 		aux = aux->next;
 	}
 }
