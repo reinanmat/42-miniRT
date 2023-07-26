f09d1dc44af0e4f11db91ab91ae551f1ebcedae3
reinanmat <reinan266@gmail.com>
2023-07-26 14:38:21 -0300

    refactor: removed init_mlx function

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,14 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 11:11:03 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/26 13:46:08 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/26 14:25:08 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
-t_mlx	init_mlx(void)
 {
-	t_mlx	mlx;
 
-	mlx_create_window(&mlx);
-	mlx_create_img(&mlx);
-	mlx_hooks(&mlx);
-	return (mlx);
 }
 
 int	main(int argc, char **argv)
@@ -30,8 +24,6 @@ int	main(int argc, char **argv)
 		exit(1);
 	else if (received_invalid_param(argv[1]))
 		exit(1);
-	data.world = init_world(argv[1]);
-	data.mlx = init_mlx();
 	render(data);
 	mlx_loop(data.mlx.mlx_ptr);
 }
