e121c1847dc6566ac394f287eae354b900d4933f
reinanmat <reinan266@gmail.com>
2023-07-26 15:46:33 -0300

    refactor: ray_color function now ray_color returns the color of the object hit or black if the ray didn't hit anything

--- srcs/ray/ray_color.c
+++ srcs/ray/ray_color.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 13:28:55 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/20 15:20:29 by revieira         ###   ########.fr       */
+/*   Updated: 2023/07/26 15:07:17 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ t_color	ray_color(t_ray ray, t_world world)
 	range.min = 0;
 	range.max = INFINITY;
 	if (hit_anything(world.objects, ray, range, &hit))
-		return (s_multiply(add(hit.normal, (t_color){1.0, 1.0, 1.0}), 0.5));
-	color = gradient_color(ray.direction.y, (t_color){1.0, 1.0, 1.0},
-			(t_color){0.5, 0.7, 1.0});
+		return (hit.color);
+	color = (t_color){0};
 	return (color);
 }
