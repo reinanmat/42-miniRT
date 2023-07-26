a5f9111b81cdb584f43b7e4c7458187f176e989d
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-26 13:38:07 -0300

    refactor: removed unused functions

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 11:11:03 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/24 19:05:45 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/26 13:37:55 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,6 @@ t_mlx	init_mlx(void)
 	mlx_hooks(&mlx);
 	return (mlx);
 }
-
-/* t_light	init_light(t_color color) */
-/* { */
-/* 	t_light	light; */
-
-/* 	light.position = (t_point){0, 0, 0}; */
-/* 	light.color = color; */
-/* 	light.brightness = 0.5; */
-/* 	return (light); */
-/* } */
-
-/* t_hittable	*add_objects(void) */
-/* { */
-/* 	t_hittable	*objects; */
-/* 	t_sphere	*sp1; */
-/* 	t_sphere	*floor; */
-
-/* 	objects = NULL; */
-/* 	sp1 = sp((t_point){0, 0, -1}, 0.75, (t_color){0, 0, 1}); */
-/* 	floor = sp((t_point){0, -1000.5, 0}, 999.5, (t_color){0, 0, 1}); */
-/* 	hittable_add(1, sp1, &objects); */
-/* 	hittable_add(1, floor, &objects); */
-/* 	return (objects); */
-/* } */
 
 int	main(int argc, char **argv)
 {
