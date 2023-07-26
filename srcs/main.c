003dfe040ba53a2192d6cce542d11afae5b3a719
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-07-26 13:49:45 -0300

    fix: was not getting the return of init_world, now I am

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 11:11:03 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/26 13:37:55 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/07/26 13:46:08 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,7 @@ int	main(int argc, char **argv)
 		exit(1);
 	else if (received_invalid_param(argv[1]))
 		exit(1);
-	init_world(argv[1]);
-	exit(0);
+	data.world = init_world(argv[1]);
 	data.mlx = init_mlx();
 	render(data);
 	mlx_loop(data.mlx.mlx_ptr);
