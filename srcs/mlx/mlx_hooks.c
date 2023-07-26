6f6dfd05bf3db5d6d29840aa49e4ee2560196398
reinanmat <reinan266@gmail.com>
2023-07-26 14:34:38 -0300

    refactor: mlx_hooks functions
    now these functions receive the struct data as a parameter

--- srcs/mlx/mlx_hooks.c
+++ srcs/mlx/mlx_hooks.c
@@ -6,27 +6,28 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 16:13:24 by revieira          #+#    #+#             */
-/*   Updated: 2023/06/19 13:32:10 by revieira         ###   ########.fr       */
+/*   Updated: 2023/07/26 14:19:07 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
 
-static int	close_program(t_mlx *mlx)
+static int	close_program(t_data *data)
 {
-	mlx_close_window(mlx);
+	mlx_close_window(&data->mlx);
+	clean_hittable(&data->world.objects);	
 	exit(0);
 }
 
-static int	handle_input(int key, t_mlx *mlx)
+static int	handle_input(int key, t_data *data)
 {
 	if (key == KEY_ESC)
-		close_program(mlx);
+		close_program(data);
 	return (0);
 }
 
-void	mlx_hooks(t_mlx *mlx)
+void	mlx_hooks(t_data *data)
 {
-	mlx_hook(mlx->win_ptr, 2, 1L << 0, &handle_input, mlx);
-	mlx_hook(mlx->win_ptr, 17, 0L, &close_program, mlx);
+	mlx_hook(data->mlx.win_ptr, 2, 1L << 0, &handle_input, data);
+	mlx_hook(data->mlx.win_ptr, 17, 0L, &close_program, data);
 }
