aeed7031c3ccd29ac00cd40ffa770cf3beb64ac5
reinanmat <reinan266@gmail.com>
2023-08-02 18:43:39 -0300

    feat: added utils functions for print matrices

--- srcs/utils_tests.c
+++ srcs/utils_tests.c
@@ -6,11 +6,34 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 12:22:10 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/25 19:46:34 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/08/02 17:39:03 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
+
+void	print_matrix(double matrix[4][4])
+{
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			printf("| ");
+			printf("%d\t", (int)matrix[i][j]);
+		}
+		printf("|\n");
+	}
+}
+
+void	print_matrix2(t_matrix matrix)
+{
+	for (int i = 0; i < matrix.rows; ++i) {
+		for (int j = 0; j < matrix.cols; ++j) {
+			printf("| ");
+			printf("%.5f ", (matrix.matr[i][j]));
+		}
+		printf("|\n");
+	}
+}
+
 
 void	print_hittable(t_hittable **hittable)
 {
