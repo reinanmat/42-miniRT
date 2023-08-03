fa49c142087fbe2f1d784ce31a022860f42737b2
reinanmat <reinan266@gmail.com>
2023-08-03 16:24:08 -0300

    fix: using wrong variable to convert matrix back to a point

--- srcs/linear_algebra/transform.c
+++ srcs/linear_algebra/transform.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:05:47 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/08/03 13:36:56 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/08/03 14:02:30 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
@@ -18,7 +18,7 @@ void	apply_transformation(t_point *pos, t_matrix transformation)
 	t_matrix	m_point;
 
 	m_point = point_to_matrix(*pos);
-	applied = multiply_matrix(m_point, transformation);
-	new_pos = point(m_point.matr[0][0], m_point.matr[1][0], m_point.matr[2][0]);
+	applied = multiply_matrix(transformation, m_point);
+	new_pos = point(applied.matr[0][0], applied.matr[1][0], applied.matr[2][0]);
 	*pos = new_pos;
 }
