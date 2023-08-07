286d22120c6c0b194294ab5afba0d9470fecd35b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-08-07 19:41:49 -0300

    feat: added put_pixel function

--- srcs/render.c
+++ srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:25:59 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/21 18:56:50 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/04 18:12:07 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,12 @@ t_color	calculate_ray_color(int x, int y, t_world world)
 	color = ray_color(ray, world);
 	return (color);
 }
+
+void	put_pixel(t_point point, int color, t_mlx mlx)
+{
+	mlx_img_pix_put(&mlx.img, point.x + ((double) WIDTH / 2), -point.y + ((double) HEIGHT / 2), color);
+}
+
 
 void	render_world(t_world world, t_mlx mlx)
 {
