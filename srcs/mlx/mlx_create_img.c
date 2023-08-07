df741d18fb55b7c203a34bf610a4f802f2e3c5d5
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-08-07 19:40:30 -0300

    fix: removed unused calculated from aspect ratio

--- srcs/mlx/mlx_create_img.c
+++ srcs/mlx/mlx_create_img.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 16:04:19 by revieira          #+#    #+#             */
-/*   Updated: 2023/06/26 20:19:31 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/08/03 17:56:19 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 void	mlx_create_img(t_mlx *mlx)
 {
-	int		height;
-
-	height = (WIDTH / 16) * 9;
-	mlx->img.mlx_img = mlx_new_image(mlx->mlx_ptr, WIDTH, height);
+	mlx->img.mlx_img = mlx_new_image(mlx->mlx_ptr, WIDTH, HEIGHT);
 	mlx->img.addr = mlx_get_data_addr(mlx->img.mlx_img, &mlx->img.bpp,
 			&mlx->img.line_len, &mlx->img.endian);
 }
