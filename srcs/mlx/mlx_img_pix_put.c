134bc48871a1335ebe355c579749924b7ce448ee
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-08-08 17:16:08 -0300

    fix: casting the color to unsigned int, since it's a hexadecimal number

--- srcs/mlx/mlx_img_pix_put.c
+++ srcs/mlx/mlx_img_pix_put.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 16:36:43 by revieira          #+#    #+#             */
-/*   Updated: 2023/06/19 14:24:10 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/04 16:32:29 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	mlx_img_pix_put(t_img *img, int x, int y, int color)
 	char	*pixel;
 
 	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
-	*(int *)pixel = color;
+	*(unsigned int *)pixel = color;
 }
