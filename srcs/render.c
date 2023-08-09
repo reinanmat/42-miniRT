44408077ec1c1d2dba58fbc27c14cc8c489fb25a
reinanmat <reinan266@gmail.com>
2023-08-09 17:38:17 -0300

    refactor: removed render functions for testing

--- srcs/render.c
+++ srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:25:59 by revieira          #+#    #+#             */
-/*   Updated: 2023/08/04 18:12:07 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/08/08 19:54:03 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,38 +24,4 @@ t_color	calculate_ray_color(int x, int y, t_world world)
 	ray = get_ray(u, v, world.cam);
 	color = ray_color(ray, world);
 	return (color);
-}
-
-void	put_pixel(t_point point, int color, t_mlx mlx)
-{
-	mlx_img_pix_put(&mlx.img, point.x + ((double) WIDTH / 2), -point.y + ((double) HEIGHT / 2), color);
-}
-
-
-void	render_world(t_world world, t_mlx mlx)
-{
-	int		x;
-	int		y;
-	t_color	ray_color;
-
-	y = HEIGHT;
-	while (y >= 0)
-	{
-		x = 0;
-		while (x <= WIDTH)
-		{
-			ray_color = calculate_ray_color(x, y, world);
-			mlx_img_pix_put(&mlx.img, x, HEIGHT - y, get_color(ray_color));
-			x++;
-		}
-		y--;
-	}
-}
-
-int	render(t_data data)
-{
-	render_world(data.world, data.mlx);
-	mlx_put_image_to_window(data.mlx.mlx_ptr, data.mlx.win_ptr,
-		data.mlx.img.mlx_img, 0, 0);
-	return (0);
 }
