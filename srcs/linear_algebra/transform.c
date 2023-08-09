2dcb29acc345e17bb1b4fd2fcf0a3f4e956c103e
reinanmat <reinan266@gmail.com>
2023-08-09 17:22:45 -0300

    feat: added apply_transformation point and vec

--- srcs/linear_algebra/transform.c
+++ srcs/linear_algebra/transform.c
@@ -6,12 +6,12 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:05:47 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/08/03 14:02:30 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/08 18:20:30 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
 
-void	apply_transformation(t_point *pos, t_matrix transformation)
+void	apply_transformation_point(t_point *pos, t_matrix transformation)
 {
 	t_point		new_pos;
 	t_matrix	applied;
@@ -21,4 +21,16 @@ void	apply_transformation(t_point *pos, t_matrix transformation)
 	applied = multiply_matrix(transformation, m_point);
 	new_pos = point(applied.matr[0][0], applied.matr[1][0], applied.matr[2][0]);
 	*pos = new_pos;
+}
+
+void	apply_transformation_vec(t_vec3 *vec, t_matrix transformation)
+{
+	t_point		new_pos;
+	t_matrix	applied;
+	t_matrix	m_point;
+
+	m_point = vec3_to_matrix(*vec);
+	applied = multiply_matrix(transformation, m_point);
+	new_pos = point(applied.matr[0][0], applied.matr[1][0], applied.matr[2][0]);
+	*vec = new_pos;
 }
