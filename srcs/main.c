7c9339627f93014b43632a97b588e73d941b6fc1
reinanmat <reinan266@gmail.com>
2023-08-17 17:37:42 -0300

    refactor: moved normal_at function from main file to normal_at file

--- srcs/main.c
+++ srcs/main.c
@@ -6,31 +6,10 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 11:11:03 by revieira          #+#    #+#             */
-/*   Updated: 2023/08/17 16:12:39 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/17 17:09:27 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/minirt.h"
-
-t_vec3	normal_at(t_hittable *object, t_point world_point)
-{
-	t_matrix	inv_matrix;
-	t_vec3		world_normal;
-	t_point		object_point;
-	t_vec3		object_normal;
-	t_matrix	normal_as_matrix;
-
-	if (object->type == 1)
-		inv_matrix = inverse(object->sp->transform);
-	if (object->type == 2)
-		inv_matrix = inverse(object->cy->transform);
-	if (object->type == 3)
-		inv_matrix = inverse(object->pl->transform);
-	object_point = matrix_to_point(multiply_matrix(inv_matrix, point_to_matrix(world_point)));
-	object_normal = sub(object_point, point(0, 0, 0));
-	normal_as_matrix = point_to_matrix(object_normal);
-	world_normal = matrix_to_point(multiply_matrix(transpose_matrix(inv_matrix), normal_as_matrix));
-	return (normalize(world_normal));
-}
 
 t_light	point_light(t_point pos, double intensity)
 {
