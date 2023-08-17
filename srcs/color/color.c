addb71a123a3d88ac03908367ab1e3398492edaf
reinanmat <reinan266@gmail.com>
2023-08-17 16:33:13 -0300

    refactor: moved fix_colors function from render file to color file

--- srcs/color/color.c
+++ srcs/color/color.c
@@ -6,11 +6,21 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:25:05 by revieira          #+#    #+#             */
-/*   Updated: 2023/07/26 15:31:23 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/17 15:55:56 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
+
+static void	fix_colors(t_color *color)
+{
+	if (color->x > 1)
+		color->x = 1;
+	if (color->y > 1)
+		color->y = 1;
+	if (color->z > 1)
+		color->z = 1;
+}
 
 int	get_color(t_color color)
 {
@@ -18,6 +28,7 @@ int	get_color(t_color color)
 	int	g;
 	int	b;
 
+	fix_colors(&color);
 	r = color.x;
 	g = color.y;
 	b = color.z;
