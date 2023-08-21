533848645b8dfeef83469ef56adc39e833e7b169
reinanmat <reinan266@gmail.com>
2023-08-21 18:45:01 -0300

    refactor: breaking the intersection_calculate function into another function

--- srcs/intersections/intersection.c
+++ srcs/intersections/intersection.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:57:42 by revieira          #+#    #+#             */
-/*   Updated: 2023/08/17 18:01:07 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/21 18:18:20 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,31 @@ t_intersections	*hit(t_intersections *intersections)
 	return (intersect);
 }
 
-t_intersections	*intersection_calculate(t_ray ray, t_hittable *objects)
+void	intersection(t_ray ray, t_hittable *obj, t_intersections **intersects)
 {
 	t_ray			tmp_ray;
-	t_intersections	*intersect;
 	t_inter_point	inter_p;
+
+	if (obj->type == 1)
+	{
+		tmp_ray = transform_ray(ray, inverse(obj->sp->transform));
+		inter_p = intersect_sphere(tmp_ray, obj->sp);
+		if (inter_p.hit_times != 0)
+		{
+			intersect_add_back(intersects, new_intersect(inter_p.hit[0], obj));
+			intersect_add_back(intersects, new_intersect(inter_p.hit[1], obj));
+		}
+	}
+}
+
+t_intersections	*intersection_calculate(t_ray ray, t_hittable *objects)
+{
+	t_intersections	*intersect;
 
 	intersect = NULL;
 	while (objects)
 	{
-		if (objects->type == 1)
-		{
-			tmp_ray = transform_ray(ray, inverse(objects->sp->transform));
-			inter_p = intersect_sphere(tmp_ray, objects->sp);
-			if (inter_p.hit_times != 0)
-			{
-				intersect_add_back(&intersect, new_intersect(inter_p.hit[0], objects));
-				intersect_add_back(&intersect, new_intersect(inter_p.hit[1], objects));
-			}
-		}
+		intersection(ray, objects, &intersect);
 		objects = objects->next;
 	}
 	sort_lst(&intersect);
