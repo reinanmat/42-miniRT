c6320d4c515b33e52ff3d52ac4c35a6567d6aef4
reinanmat <reinan266@gmail.com>
2023-08-28 16:19:06 -0300

    refactor: moved get_material and get_color from lighting file to hittable_getters file

---
+++ srcs/structures/hittable_getters.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hittable_getters.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 16:13:46 by revieira          #+#    #+#             */
+/*   Updated: 2023/08/28 16:14:16 by revieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../includes/minirt.h"
+
+t_color	get_color(t_hittable *object)
+{
+	if (object->type == 1)
+		return (object->sp->material.color);
+	else if (object->type == 2)
+		return (object->cy->material.color);
+	else
+		return (object->pl->material.color);
+}
+
+t_material	get_material(t_hittable *object)
+{
+	if (object->type == 1)
+		return (object->sp->material);
+	else if (object->type == 2)
+		return (object->cy->material);
+	else
+		return (object->pl->material);
+}
