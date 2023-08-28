e0a07a6ad45dc6b3167532b55e9a4ac60c24612a
reinanmat <reinan266@gmail.com>
2023-08-28 12:36:46 -0300

    feat: added normalize_color function

--- srcs/color/color.c
+++ srcs/color/color.c
@@ -6,10 +6,20 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:25:05 by revieira          #+#    #+#             */
-/*   Updated: 2023/08/21 18:53:00 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/28 11:58:47 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
+
+t_color	normalize_color(t_color	color)
+{
+	t_color	normalized_color;
+
+	normalized_color.x = color.x / 255;
+	normalized_color.y = color.y / 255;
+	normalized_color.z = color.z / 255;
+	return (normalized_color);
+}
 
 static void	fix_colors(t_color *color)
 {
