b4ad1310533f8d55bb0a670c2d7f85f083eb39a9
reinanmat <reinan266@gmail.com>
2023-08-28 17:33:17 -0300

    refactor: moved init_camera function from init_world file to camera file

--- srcs/world/camera.c
+++ srcs/world/camera.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 20:29:27 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/08/28 16:31:08 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/28 17:13:35 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
@@ -29,4 +29,20 @@ void	set_pixel_size(t_cam *camera)
 		camera->viewport_height = half_view;
 	}
 	camera->pixel_size = (camera->viewport_width * 2) / WIDTH;
+}
+
+t_cam	init_camera(char **lines)
+{
+	t_cam	cam;
+	char	**config;
+
+	config = get_splitted_identifier(lines, "C");
+	assign_t_point(&cam.coordinate, config[1]);
+	assign_t_point(&cam.orientation_vec, config[2]);
+	cam.fov = degress_to_radians(ft_atof(config[3]));
+	cam.fov = M_PI / 3;
+	set_pixel_size(&cam);
+	cam.t = view_transform(cam.coordinate, cam.orientation_vec, vec3(0, 1, 0));
+	ft_free_matrix((void **)config);
+	return (cam);
 }
--- srcs/world/init_world.c
+++ srcs/world/init_world.c
@@ -11,30 +11,6 @@
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
 
-static t_cam	init_cam(char **lines)
-{
-	t_cam	cam;
-	char	**config;
-	double	aspect_ratio;
-	double	vertical_fov;
-	double	vertical_fov_radians;
-
-	config = get_splitted_identifier(lines, "C");
-	vertical_fov = ft_atof(config[3]);
-	vertical_fov_radians = vertical_fov * M_PI / 180.0;
-	aspect_ratio = 16.0 / 9.0;
-	cam.viewport_height = 2.0 * tan(vertical_fov_radians / 2.0);
-	cam.viewport_width = cam.viewport_height * aspect_ratio;
-	cam.focal_length = 1.0;
-	assign_t_point(&cam.coordinate, config[1]);
-	assign_t_point(&cam.orientation_vec, config[2]);
-	cam.horizontal = (t_point){cam.viewport_width, 0, 0};
-	cam.vertical = (t_point){0, cam.viewport_height, 0};
-	cam.lower_left_corner = get_lower_left_corner(cam);
-	ft_free_matrix((void **)config);
-	return (cam);
-}
-
 static t_amb_light	init_ambient_light(char **lines)
 {
 	t_amb_light	amb_light;
@@ -71,7 +47,7 @@ t_world	init_world(char *filename)
 	lines = ft_split(file_content, '\n');
 	free(file_content);
 	trim_lines(lines);
-	world.cam = init_cam(lines);
+	world.cam = init_camera(lines);
 	world.ambient_light = init_ambient_light(lines);
 	world.light = init_light(lines);
 	world.objects = init_objects(lines);
