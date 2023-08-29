0da4abc27fcf607a9574b74621fe50ff0f9b50ae
reinanmat <reinan266@gmail.com>
2023-08-29 18:28:29 -0300

    refactor: passing vec3 forward as parameter

--- srcs/linear_algebra/matrices/view_transformation.c
+++ srcs/linear_algebra/matrices/view_transformation.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 18:39:49 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/08/23 20:05:26 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/08/29 18:01:52 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
@@ -18,15 +18,13 @@ static void	set_values_on_given_col(t_matrix *matrix, int col, t_vec3 a)
 	matrix->matr[col][2] = a.z;
 }
 
-t_matrix	view_transform(t_vec3 from, t_vec3 to, t_vec3 up)
+t_matrix	view_transform(t_vec3 from, t_vec3 forward, t_vec3 up)
 {
-	t_vec3		forward;
 	t_vec3		left;
 	t_vec3		true_up;
 	t_matrix	orientation_matrix;
 
 	orientation_matrix = identity_matrix();
-	forward = normalize(sub(to, from));
 	left = cross_product(forward, normalize(up));
 	true_up = cross_product(left, forward);
 	set_values_on_given_col(&orientation_matrix, 0, left);
