a6c431550592ed5752272e6c6e17b522f9d5f1d0
reinanmat <reinan266@gmail.com>
2023-08-30 17:21:50 -0300

    refactor: new HEIGHT

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:32:12 by revieira          #+#    #+#             */
-/*   Updated: 2023/08/28 15:57:52 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/30 17:20:44 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define MINIRT_H
 
 # define WIDTH 720
-# define HEIGHT 360
+# define HEIGHT 480
 # define WINDOW_NAME "minirt"
 
 # define KEY_ESC 0xff1b
