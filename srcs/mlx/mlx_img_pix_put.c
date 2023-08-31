d0b5d3989b73ff20e2afe4c2b08e6ab15a879fa0
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-08-30 21:23:45 -0300

    fix: add mlx_img_pix_put protection

--- srcs/mlx/mlx_img_pix_put.c
+++ srcs/mlx/mlx_img_pix_put.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 16:36:43 by revieira          #+#    #+#             */
-/*   Updated: 2023/08/21 18:54:23 by revieira         ###   ########.fr       */
+/*   Updated: 2023/08/30 19:17:47 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
@@ -16,5 +16,7 @@ void	mlx_img_pix_put(t_img *img, int x, int y, int color)
 	char	*pixel;
 
 	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	if (x > WIDTH || y > HEIGHT || y < 0 || x < 0)
+		return ;
 	*(unsigned int *)pixel = color;
 }
