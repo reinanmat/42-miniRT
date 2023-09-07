8a521355930949c170c59bde6d46a761fd231e4b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-09-07 12:20:53 -0300

    fixed norminette view_transformation

--- srcs/linear_algebra/matrices/view_transformation.c
+++ srcs/linear_algebra/matrices/view_transformation.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 18:39:49 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/08/29 18:01:52 by revieira         ###   ########.fr       */
+/*   Updated: 2023/09/07 12:20:26 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt.h"
@@ -22,13 +22,15 @@ t_matrix	view_transform(t_vec3 from, t_vec3 forward, t_vec3 up)
 {
 	t_vec3		left;
 	t_vec3		true_up;
-	t_matrix	orientation_matrix;
+	t_vec3		negated_vec;
+	t_matrix	orientation_matr;
 
-	orientation_matrix = identity_matrix();
+	negated_vec = s_multiply(from, -1);
+	orientation_matr = identity_matrix();
 	left = cross_product(forward, normalize(up));
 	true_up = cross_product(left, forward);
-	set_values_on_given_col(&orientation_matrix, 0, left);
-	set_values_on_given_col(&orientation_matrix, 1, true_up);
-	set_values_on_given_col(&orientation_matrix, 2, s_multiply(forward, -1));
-	return (multiply_matrix(orientation_matrix, translation_matrix(s_multiply(from, -1))));
+	set_values_on_given_col(&orientation_matr, 0, left);
+	set_values_on_given_col(&orientation_matr, 1, true_up);
+	set_values_on_given_col(&orientation_matr, 2, s_multiply(forward, -1));
+	return (multiply_matrix(orientation_matr, translation_matrix(negated_vec)));
 }
