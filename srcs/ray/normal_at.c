e314705d5d85882bdc8ee8f2c4b0b6483ec8bb03
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-09-07 12:17:57 -0300

    style: fixed norminette srcs/ray/normal_at

--- srcs/ray/normal_at.c
+++ srcs/ray/normal_at.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 17:05:42 by revieira          #+#    #+#             */
-/*   Updated: 2023/09/06 19:19:13 by revieira         ###   ########.fr       */
+/*   Updated: 2023/09/07 12:16:00 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
@@ -34,6 +34,30 @@ t_vec3	local_normal_at(t_hittable *object, t_point local_point)
 		return (vec3(0, 1, 0));
 }
 
+t_point	get_object_point(t_matrix inv_matrix, t_point world_point)
+{
+	t_point		object_point;
+	t_matrix	world_point_matrix;
+	t_matrix	inv_by_world_point;
+
+	world_point_matrix = point_to_matrix(world_point);
+	inv_by_world_point = multiply_matrix(inv_matrix, world_point_matrix);
+	object_point = matrix_to_point(inv_by_world_point);
+	return (object_point);
+}
+
+t_vec3	get_world_normal(t_matrix inv_matrix, t_matrix normal_as_matrix)
+{
+	t_matrix	transposed_matrix;
+	t_matrix	transpose_by_normal;
+	t_vec3		world_normal;
+
+	transposed_matrix = transpose_matrix(inv_matrix);
+	transpose_by_normal = multiply_matrix(transposed_matrix, normal_as_matrix);
+	world_normal = matrix_to_point(transpose_by_normal);
+	return (world_normal);
+}
+
 t_vec3	normal_at(t_hittable *object, t_point world_point)
 {
 	t_matrix	inv_matrix;
@@ -43,9 +67,9 @@ t_vec3	normal_at(t_hittable *object, t_point world_point)
 	t_matrix	normal_as_matrix;
 
 	inv_matrix = inverse(get_transform(object));
-	object_point = matrix_to_point(multiply_matrix(inv_matrix, point_to_matrix(world_point)));
+	object_point = get_object_point(inv_matrix, world_point);
 	object_normal = local_normal_at(object, object_point);
 	normal_as_matrix = point_to_matrix(object_normal);
-	world_normal = matrix_to_point(multiply_matrix(transpose_matrix(inv_matrix), normal_as_matrix));
+	world_normal = get_world_normal(inv_matrix, normal_as_matrix);
 	return (normalize(world_normal));
 }
