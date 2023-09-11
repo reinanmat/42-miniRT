7a93c807097bcec7e0d7612dd4bae131e81e222a
reinanmat <reinan266@gmail.com>
2023-09-11 15:00:37 -0300

    refactor: restoring ambient color to default

--- srcs/world/material.c
+++ srcs/world/material.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 16:55:37 by revieira          #+#    #+#             */
-/*   Updated: 2023/09/11 12:02:27 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/09/11 14:27:15 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
@@ -15,7 +15,7 @@ t_material	default_material(void)
 {
 	t_material	m;
 
-	m.ambient = 1;
+	m.ambient = 0.1;
 	m.diffuse = 0.9;
 	m.specular = 0.9;
 	m.shininess = 200.0;
