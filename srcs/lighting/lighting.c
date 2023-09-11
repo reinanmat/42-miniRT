500a944aa22cd1a9759da7965c550d0b50c49c54
reinanmat <reinan266@gmail.com>
2023-09-11 16:17:31 -0300

    fix: norminette

--- srcs/lighting/lighting.c
+++ srcs/lighting/lighting.c
@@ -22,27 +22,25 @@ static t_color	compute_color(t_color colors[3], t_comps comps, t_amb_light amb)
 	return (color);
 }
 
-t_color	lighting(t_light light, t_comps comps, t_world world, int is_shadowed)
+t_color	lighting(t_light light, t_comps comps, t_amb_light amb, int shadow)
 {
-	t_color		ambient;
-	t_color		diffuse;
-	t_color		specular;
+	t_color		colors[3];
 	double		reflect_dot_eye;
 	double		light_dot_normal;
 
 	reflect_dot_eye = 0.0;
-	diffuse = (t_color){0, 0, 0};
-	specular = (t_color){0, 0, 0};
-	ambient = compute_ambient(light, comps);
-	if (is_shadowed)
-		return (compute_color(ambient, diffuse, specular, comps, world));
+	colors[DIFFUSE] = (t_color){0, 0, 0};
+	colors[SPECULAR] = (t_color){0, 0, 0};
+	colors[AMBIENT] = compute_ambient(light, comps);
+	if (shadow)
+		return (compute_color(colors, comps, amb));
 	light_dot_normal = compute_light_dot_normal(light, comps);
 	if (light_dot_normal >= 0)
 	{
-		diffuse = compute_diffuse(light, light_dot_normal, comps);
+		colors[DIFFUSE] = compute_diffuse(light, light_dot_normal, comps);
 		reflect_dot_eye = compute_reflect_dot_eye(light, comps);
 	}
 	if (reflect_dot_eye > 0)
-		specular = compute_specular(reflect_dot_eye, comps, light);
-	return (compute_color(ambient, diffuse, specular, comps, world));
+		colors[SPECULAR] = compute_specular(reflect_dot_eye, comps, light);
+	return (compute_color(colors, comps, amb));
 }
