0d60adc2ab4af3cd10b5c61262ae4d316e31248d
reinanmat <reinan266@gmail.com>
2023-09-13 17:28:17 -0300

    feat: initialize has_pattern

--- bonus/srcs/world/material.c
+++ bonus/srcs/world/material.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 16:55:37 by revieira          #+#    #+#             */
-/*   Updated: 2023/09/13 14:43:16 by revieira         ###   ########.fr       */
+/*   Updated: 2023/09/13 15:42:21 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt_bonus.h"
@@ -19,6 +19,7 @@ t_material	default_material(void)
 	m.diffuse = 0.9;
 	m.specular = 0.9;
 	m.shininess = 200.0;
+	m.has_pattern = 0;
 	m.pattern = stripe_pattern(color(1, 1, 1), color(0, 0, 0));
 	return (m);
 }
