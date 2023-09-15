ca02220dd44e61829a96601991bc22d14150da14
reinanmat <reinan266@gmail.com>
2023-09-15 20:44:15 -0300

    refactor: added multiple light in world structure, possible leaks

--- bonus/srcs/world/init_world.c
+++ bonus/srcs/world/init_world.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 17:17:17 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/09/12 17:57:14 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/09/15 20:35:56 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt_bonus.h"
@@ -23,16 +23,45 @@ static t_amb_light	init_ambient_light(char **lines)
 	return (amb_light);
 }
 
-static t_light	init_light(char **lines)
+static int	get_amount_of_lights(char **config)
 {
-	t_light	light;
+	int	i;
+	int	amount;
+
+	i = 0;
+	amount = 0;
+	while (config[i])
+	{
+		if (config[i][0] == 'L')
+			amount++;
+		i++;
+	}
+	return (amount);
+}
+
+static t_light	*init_light(int number_of_lights, char **lines)
+{
+	int		i;
+	int		j;
+	t_light	*lights;
 	char	**config;
 
-	config = get_splitted_identifier(lines, "L");
-	light.brightness = ft_atof(config[2]);
-	assign_t_point(&light.coordinate, config[1]);
-	ft_free_matrix((void **)config);
-	return (light);
+	lights = ft_calloc(sizeof(t_light), number_of_lights + 1);
+	i = 0;
+	j = 0;
+	while (lines[i] && j < number_of_lights)
+	{
+		if (lines[i][0] == 'L')
+		{
+			config = get_splitted_identifier(lines, "L");
+			lights[j].brightness = ft_atof(config[2]);
+			assign_t_point(&lights[j].coordinate, config[1]);
+			ft_free_matrix((void **)config);
+			j++;
+		}
+		i++;
+	}
+	return (lights);
 }
 
 t_world	init_world(char *filename)
@@ -49,7 +78,8 @@ t_world	init_world(char *filename)
 	trim_lines(lines);
 	world.cam = init_camera(lines);
 	world.ambient_light = init_ambient_light(lines);
-	world.light = init_light(lines);
+	world.amount_of_lights = get_amount_of_lights(lines);
+	world.light = init_light(world.amount_of_lights, lines);
 	world.objects = init_objects(lines);
 	ft_free_matrix((void **)lines);
 	return (world);
