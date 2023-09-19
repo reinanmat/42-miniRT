b18dc45e0569e45b8d5048cff4833c166d559b9f
reinanmat <reinan266@gmail.com>
2023-09-19 15:29:53 -0300

    fix: norminette

--- mandatory/srcs/lighting_and_shadow/lighting.c
+++ mandatory/srcs/lighting_and_shadow/lighting.c
@@ -6,41 +6,41 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 15:23:43 by revieira          #+#    #+#             */
-/*   Updated: 2023/09/19 13:56:24 by revieira         ###   ########.fr       */
+/*   Updated: 2023/09/19 15:28:09 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minirt.h"
 
-static double	compute_light_dot_normal(t_light light, t_comps comps)
+static double	compute_light_dot_normal(t_light l, t_comps comps)
 {
 	t_vec3	lightv;
 	double	light_dot_normal;
 
-	lightv = normalize(sub(light.coordinate, comps.over_point));
+	lightv = normalize(sub(l.coordinate, comps.over_point));
 	light_dot_normal = dot(lightv, comps.normalv);
 	return (light_dot_normal);
 }
 
-static t_color	compute_diffuse(t_light light, double light_dot_normal, t_comps comps)
+static t_color	compute_diffuse(t_light l, double light_dot_nrml, t_comps comps)
 {
 	t_color		diffuse;
 	t_color		effective_color;
 	t_material	material;
 
 	material = get_material(comps.object);
-	effective_color = s_multiply(get_color(comps.object), light.brightness);
-	diffuse = s_multiply(effective_color, material.diffuse * light_dot_normal);
+	effective_color = s_multiply(get_color(comps.object), l.brightness);
+	diffuse = s_multiply(effective_color, material.diffuse * light_dot_nrml);
 	return (diffuse);
 }
 
-static t_color	compute_ambient(t_light light, t_comps comps)
+static t_color	compute_ambient(t_light l, t_comps comps)
 {
 	t_color		ambient;
 	t_color		effective_color;
 	t_material	material;
 
 	material = get_material(comps.object);
-	effective_color = s_multiply(get_color(comps.object), light.brightness);
+	effective_color = s_multiply(get_color(comps.object), l.brightness);
 	ambient = s_multiply(effective_color, material.ambient);
 	return (ambient);
 }
