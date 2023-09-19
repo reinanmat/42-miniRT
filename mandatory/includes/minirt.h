4fc928e2860fa63084e53a65615f3ed1018939e8
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-09-19 19:48:47 -0300

    style: norminette

--- mandatory/includes/minirt.h
+++ mandatory/includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:32:12 by revieira          #+#    #+#             */
-/*   Updated: 2023/09/19 17:24:28 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/09/19 19:48:34 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,14 +64,14 @@ typedef struct s_data
 }	t_data;
 
 //render
-int		render(t_data data);
+int			render(t_data data);
 
 //mlx functions
-void	mlx_close_window(t_mlx *mlx);
-void	mlx_create_img(t_mlx *mlx);
-void	mlx_create_window(t_mlx *mlx);
-void	mlx_hooks(t_data *data);
-void	mlx_img_pix_put(t_img *img, int x, int y, int color);
+void		mlx_close_window(t_mlx *mlx);
+void		mlx_create_img(t_mlx *mlx);
+void		mlx_create_window(t_mlx *mlx);
+void		mlx_hooks(t_data *data);
+void		mlx_img_pix_put(t_img *img, int x, int y, int color);
 
 //utils
 int			reset_fd(int fd, char *filename);
@@ -84,39 +84,39 @@ int			check_equal_vec3(t_vec3 a, t_vec3 b);
 t_matrix	get_orientation(t_vec3 a);
 
 //file parsing
-int		received_invalid_param(char *filename);
-int		expected_number_of_identifiers(char **lines);
-void	trim_lines(char **lines);
-int		map_parsed_successfully(char **lines);
-int		is_valid_identifier(char *identifier);
-int		is_valid_ambient_lighting(char **params, int number_of_params);
-int		is_valid_camera(char **params, int number_of_params);
-int		is_valid_light(char **params, int number_of_params);
-int		is_valid_sphere(char **params, int number_of_params);
-int		is_valid_plane(char **params, int number_of_params);
-int		is_valid_cylinder(char **params, int number_of_params);
-int		is_number(const char *str);
-int		set_contain_only_numbers(const char *str);
-int		is_valid_rgb_set(const char *str);
-int		is_valid_lighting_ratio(const char *str);
-int		is_valid_coordinates(const char *str);
-int		expected_number_of_fields(const char *str, int expected_fields);
-int		is_valid_3d_normalized_vec(const char *str);
-int		count_dots(const char *str);
+int			received_invalid_param(char *filename);
+int			expected_number_of_identifiers(char **lines);
+void		trim_lines(char **lines);
+int			map_parsed_successfully(char **lines);
+int			is_valid_identifier(char *identifier);
+int			is_valid_ambient_lighting(char **params, int number_of_params);
+int			is_valid_camera(char **params, int number_of_params);
+int			is_valid_light(char **params, int number_of_params);
+int			is_valid_sphere(char **params, int number_of_params);
+int			is_valid_plane(char **params, int number_of_params);
+int			is_valid_cylinder(char **params, int number_of_params);
+int			is_number(const char *str);
+int			set_contain_only_numbers(const char *str);
+int			is_valid_rgb_set(const char *str);
+int			is_valid_lighting_ratio(const char *str);
+int			is_valid_coordinates(const char *str);
+int			expected_number_of_fields(const char *str, int expected_fields);
+int			is_valid_3d_normalized_vec(const char *str);
+int			count_dots(const char *str);
 
 //linear algebra
-t_ray	transform_ray(t_ray	ray, t_matrix transform);
-t_vec3	reflect(t_vec3 in, t_vec3 normal);
-t_vec3	normal_at(t_hittable *object, t_point world_point);
-t_vec3	local_normal_at(t_hittable *object, t_point local_point);
+t_ray		transform_ray(t_ray	ray, t_matrix transform);
+t_vec3		reflect(t_vec3 in, t_vec3 normal);
+t_vec3		normal_at(t_hittable *object, t_point world_point);
+t_vec3		local_normal_at(t_hittable *object, t_point local_point);
 
-t_color	color_at(t_ray ray, t_world world);
+t_color		color_at(t_ray ray, t_world world);
 
 //lighting and shadow
-t_color	shade_hit(t_world world, t_comps comps);
-t_color	lighting(t_light light, t_comps comps, t_amb_light amb, int shadow);
-t_light	point_light(t_point pos, double intensity);
-int		is_shadowed(t_world world, t_point point);
+t_color		shade_hit(t_world world, t_comps comps);
+t_color		lighting(t_light light, t_comps comps, t_amb_light amb, int shadow);
+t_light		point_light(t_point pos, double intensity);
+int			is_shadowed(t_world world, t_point point);
 
 //utils tests - delet
 void	timer(void);
