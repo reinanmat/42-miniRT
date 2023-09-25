5c8b9686fa605482189b710027b1f19fdb90b1b7
reinanmat <reinan266@gmail.com>
2023-09-25 13:00:31 -0300

    fix: rendering world from file

--- mandatory/srcs/render.c
+++ mandatory/srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:25:59 by revieira          #+#    #+#             */
-/*   Updated: 2023/09/19 14:49:10 by revieira         ###   ########.fr       */
+/*   Updated: 2023/09/25 12:59:25 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/minirt.h"
@@ -78,7 +78,7 @@ static void	render_world(t_world world, t_mlx mlx)
 
 int	render(t_data data)
 {
-	render_world(complex_world(), data.mlx);
+	render_world(data.world, data.mlx);
 	mlx_put_image_to_window(data.mlx.mlx_ptr, data.mlx.win_ptr,
 		data.mlx.img.mlx_img, 0, 0);
 	return (0);
