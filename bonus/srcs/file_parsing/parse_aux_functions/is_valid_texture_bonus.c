7a99b674a060171affaae25eb900e9e85b00aee7
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-09-25 19:55:19 -0300

    feat: validating texture at the parsing part of the program

--- bonus/srcs/file_parsing/parse_aux_functions/is_valid_texture_bonus.c
+++ bonus/srcs/file_parsing/parse_aux_functions/is_valid_texture_bonus.c
@@ -6,16 +6,23 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:31:24 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/09/21 18:35:58 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:51:56 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt_bonus.h"
 
 int	is_valid_texture(char *str)
 {
-	if (ft_strcmp(str, "DOGUINHO") == 0)
-		return (1);
-	else if (ft_strcmp(str, "WORLD") == 0)
-		return (1);
-	return (0);
+	int		fd;
+	char	*file;
+
+	file = get_corresponding_ppm(str);
+	if (!file)
+		return (0);
+	fd = open(file, O_RDONLY);
+	free(file);
+	if (fd == -1)
+		return (0);
+	close(fd);
+	return (1);
 }
