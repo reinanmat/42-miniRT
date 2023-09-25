e43e2f3acd0dc5c481fa3b59b0167c3a709aa5ed
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-09-25 19:02:10 -0300

    fix: removed print statements from rgb validation

--- bonus/srcs/file_parsing/parse_aux_functions/is_valid_rgb_set_bonus.c
+++ bonus/srcs/file_parsing/parse_aux_functions/is_valid_rgb_set_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 18:25:07 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/09/12 17:55:21 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:00:16 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt_bonus.h"
@@ -36,10 +36,10 @@ static int	between_expected_range(const char *str)
 int	is_valid_rgb_set(const char *str)
 {
 	if (!expected_number_of_fields(str, 3))
-		return (ft_putstr("[RGB] Unexpected number of params\n", 0));
+		return (0);
 	else if (!set_contain_only_numbers(str))
-		return (ft_putstr("[RGB] Set does not contain only numbers\n", 0));
+		return (0);
 	else if (!between_expected_range(str))
-		return (ft_putstr("[RGB] Set not between expected range\n", 0));
+		return (0);
 	return (1);
 }
