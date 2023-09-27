c257a1144e91ede553f5e701f7a6616172f955af
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-09-27 20:13:14 -0300

    feat: [bonus] final adjustments parsing

--- bonus/srcs/file_parsing/parse_aux_functions/is_valid_cylinder_bonus.c
+++ bonus/srcs/file_parsing/parse_aux_functions/is_valid_cylinder_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:59:12 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/09/25 19:03:56 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/09/27 19:47:32 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt_bonus.h"
@@ -21,8 +21,12 @@ int	is_valid_cylinder(char **params, int number_of_params)
 		return (0);
 	else if (!is_number(params[3]))
 		return (0);
+	else if (ft_atof(params[3]) <= 0)
+		return (ft_putstr("[CY DIAM] Should be positive integer\n", 0));
 	else if (!is_number(params[4]))
 		return (0);
+	else if (ft_atof(params[4]) <= 0)
+		return (ft_putstr("[CY HEIGHT] Should be positive integer\n", 0));
 	else if (!is_valid_material_type(params[5]))
 		return (ft_putstr("[CY-MTYPE] Invalid material type\n", 0));
 	return (1);
--- bonus/srcs/file_parsing/parse_aux_functions/is_valid_sphere_bonus.c
+++ bonus/srcs/file_parsing/parse_aux_functions/is_valid_sphere_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:56:56 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/09/25 19:03:43 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/09/27 19:47:06 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../../includes/minirt_bonus.h"
@@ -19,6 +19,8 @@ int	is_valid_sphere(char **params, int number_of_params)
 		return (0);
 	else if (!is_number(params[2]))
 		return (ft_putstr("[SPH DIAM] NaN\n", 0));
+	else if (ft_atof(params[2]) <= 0)
+		return (ft_putstr("[SPH DIAM] Should be positive integer\n", 0));
 	else if (!is_valid_material_type(params[3]))
 		return (ft_putstr("[SPH-MTYPE] Invalid material type\n", 0));
 	return (1);
